0e2e3b26286b007333b428f03499eebab17d1e20
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-04-11 03:48:41 +0530

    justadd

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,82 +1,61 @@
 // Book class : Represent a Book
 
 class Book {
-
-    constructor(title, author, ISBN) {
-
-        this.title = title;
-        this.author = author;
-        this.ISBN = ISBN;
-
-    }
-
+  constructor(title, author, ISBN) {
+    this.title = title;
+    this.author = author;
+  }
 }
-
 
 // UI Class : Hanfle UI tasks
 
-
 class UI {
+  static displayBooks() {
+    const StoreBooks = [
+      {
+        title: "Harry potter",
+        author: "J.K.Rowing",
+        ISBN: "BK12P31",
+      },
+      {
+        title: "Lord of the rings",
+        author: "K.Honn",
+        ISBN: "BK00U81",
+      },
+    ];
 
-    static displayBooks() {
+    const books = StoreBooks;
 
-        const StoreBooks = [
-            {
-                title: 'Harry potter',
-                author: 'J.K.Rowing',
-                ISBN: 'BK12P31'
+    //books.forEach((book) => UI.addBookToList(book));
 
-            },
-            {
-                title: 'Lord of the rings',
-                author: 'K.Honn',
-                ISBN: 'BK00U81'
+    books.forEach(myFunction);
 
-            }
-        ];
-
-        const books = StoreBooks;
-
-        //books.forEach((book) => UI.addBookToList(book));
-
-        books.forEach(myFunction);
-
-        function myFunction(book) {
-
-            UI.addBookToList(book);
-
-        }
-
-
+    function myFunction(book) {
+      UI.addBookToList(book);
     }
+  }
 
-    static addBookToList(book) {
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+    const row = document.createElement("tr");
 
-        const list = document.querySelector('#book-list');
-        const row = document.createElement('tr');
-
-        row.innerHTML = `
+    row.innerHTML = `
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.ISBN}</td>
             <td><i class="material-icons smaller teal-text ">delete</i></td>
         `;
 
-        list.appendChild(row);
-    }
-
+    list.appendChild(row);
+  }
 }
 
 // Store Class : Handles Storage
 
-
 // Event : Display Books
 
-document.addEventListener('DOMContentLoaded', UI.displayBooks);
-
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Event : Add a Book
 
 //Event : Remove a Book
-
-
