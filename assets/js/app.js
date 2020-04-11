1197df29eeba2818452ec44096cd38173adce522
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-04-11 18:28:58 +0530

    half done

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,13 +27,14 @@ class UI {
 
     const books = StoreBooks;
 
-    //books.forEach((book) => UI.addBookToList(book));
+    books.forEach((book) => UI.addBookToList(book));
 
-    books.forEach(myFunction);
+    /*books.forEach(myFunction);
 
     function myFunction(book) {
       UI.addBookToList(book);
     }
+    */
   }
 
   static addBookToList(book) {
@@ -44,10 +45,34 @@ class UI {
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.ISBN}</td>
-            <td><i class="material-icons smaller teal-text ">delete</i></td>
+            <td><i class="material-icons smaller teal-text delete">delete</i></td>
         `;
 
     list.appendChild(row);
+  }
+
+  static deleteBook(ET) {
+    if (ET.classList.contains("delete")) {
+      let choice = prompt("Do you want to remove the Book?");
+
+      if (
+        choice == "Y" ||
+        choice == "y" ||
+        choice == "Yes" ||
+        choice == "YES" ||
+        choice == "yes"
+      ) {
+        ET.parentElement.parentElement.remove();
+      } else {
+        alert("You says NO");
+      }
+    }
+  }
+
+  static clearFields() {
+    document.querySelector("#Btitle").value = "";
+    document.querySelector("#Bauthor").value = "";
+    document.querySelector("#BISBN").value = "";
   }
 }
 
@@ -59,4 +84,44 @@ document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Event : Add a Book
 
+document
+  .getElementById("sumbitBTN")
+  .addEventListener("click", function (Event) {
+    //get book details
+    let book_ISBN =
+      Event.target.parentElement.parentElement.previousElementSibling
+        .lastElementChild.lastElementChild.lastElementChild.value;
+
+    let book_Author =
+      Event.target.parentElement.parentElement.previousElementSibling
+        .previousElementSibling.lastElementChild.lastElementChild
+        .lastElementChild.value;
+
+    let book_name =
+      Event.target.parentElement.parentElement.previousElementSibling
+        .previousElementSibling.previousElementSibling.lastElementChild
+        .lastElementChild.lastElementChild.value;
+
+    //validate inputs
+
+    if (book_ISBN === "" || book_Author === "" || book_name === "") {
+      alert("Fill all the fields");
+    } else {
+      //instatiate book
+      const book = new Book(book_name, book_Author, book_ISBN);
+
+      //add boook to UI
+      UI.addBookToList(book);
+
+      //clear field
+      UI.clearFields();
+    }
+  });
+
 //Event : Remove a Book
+
+document
+  .getElementById("book-list")
+  .addEventListener("click", function (Event) {
+    UI.deleteBook(Event.target);
+  });
